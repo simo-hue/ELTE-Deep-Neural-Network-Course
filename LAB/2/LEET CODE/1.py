587f6004eda1713dabcfa90f4375c01ca0c03b66
simo-hue <mattioli.simone.10@gmail.com>
2026-09-15 10:50:28 +0200

    mandatory exercises

--- LAB/2/LEET CODE/1.py
+++ LAB/2/LEET CODE/1.py
@@ -1,19 +1,50 @@
-class Solution(object):
-    def runningSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        counter = []
-        sum = 0
+def isAnagram(a, b):
+        if(len(a) != len(b)):
+            return False
+        
+        counter_a = {}
+        for char in list(a):
+            if char in counter_a.keys():
+                counter_a[char] += 1
+            else:
+                counter_a[char] = 1
+                
+        counter_b = {}
+        for char in list(b):
+            if char in counter_b.keys():
+                counter_b[char] += 1
+            else:
+                counter_b[char] = 1
+                
+        return counter_a == counter_b
+        
+def caller(strs):
+    res = []
 
-        if(len(nums) < 1):
-            return 0
+    if len(strs) < 1:
+        return [[""]]
+    elif len(strs) == 1:
+        return [[strs]]
 
-        counter = [nums[]]
+    for i in range(len(strs) - 1):
+        if(strs[i] in res):
+            break
+        
+        partial = [strs[i]]
+            
+        for j in range(i + 1, len(strs), 1):
+            if(isAnagram(strs[i], strs[j]) and strs[j] not in res):
+                partial.append(strs[j])
 
-        for elem in nums:
-            sum += elem
-            counter.append(sum)
+        res.append(partial)
 
-        return counter
+    return res
+ 
+           
+a = ["eat","tea","tan","ate","nat","bat"]
+b = [""]
+c = ["a"]
+
+print(f"{caller(a)}")
+print(f"{caller(b)}")
+print(f"{caller(c)}")
